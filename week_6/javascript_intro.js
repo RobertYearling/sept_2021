83eb5b0d286a87e0f2c1b625299557fd215fbd19
Rob <rob@askrob.guru>
2021-10-05 22:02:50 -0500

    Code from Week Six Lecture

--- week_6/javascript_intro.js
+++ week_6/javascript_intro.js
@@ -1,2 +1,28 @@
-var x = 5
-console.log(x);
+var number = 6
+console.log(number);
+number = number + 5
+console.log(number);
+
+
+var strOne = 'Hello '
+var strTwo = "World! "
+var strThree = 'Rob'
+console.log(strOne + strTwo + strThree) // concatination
+
+var str = 'Its been a "great" night of coding'
+console.log(str);
+
+var bool = false
+
+var number = 6
+console.log("First Value " + number);
+number = number + 5
+console.log("Second Value " + number);
+
+var arr = ['Rob', 'Yearling', 29 ]
+arr.push('Oak Park', 'IL', 60304)
+console.log(arr);
+arr.pop()
+console.log(arr);
+
+
